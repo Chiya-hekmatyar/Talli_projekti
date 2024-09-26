75c2f0df4762f84790a6acf4f0f2fdaf4ea1a398
Chiya-hekmatyar <chiya.hekmatyar@edu.omnia.fi>
2024-09-26 16:11:10 +0300

    pois

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Production Index page</title>
+  <link rel="stylesheet" type="text/css" href="site.css">
+</head>
+
+
+<body>
+  
+<nav>
+    <div class="navbar">
+      <div><a href="index.php">Home</a></div>
+              <div><a href="login.php">Login</a></div>
+        <div><a href="register.php">Register</a></div>
+            </div>
+
+
+
+
+  <h1>Production Index page</h1>
+  <h3 class='info'>You are in production now.</h3> 
+  <p>This is a test version of the cardgame creator</p>
+
+  
+
+  <footer>
+  This is a very simple test site fetched and running at: 2024-09-26 12:59:31  </footer>
+
+</body>
