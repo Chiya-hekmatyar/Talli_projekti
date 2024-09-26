ac8afa72ddaf5447f792fbe2d231d6e0a1f6e152
Chiya-hekmatyar <chiya.hekmatyar@edu.omnia.fi>
2024-09-26 15:54:06 +0300

    Add files via upload

---
+++ etusivu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<?php
+$title = "Etusivu";
+include 'head.php';
+?>
+<body>
+<?php include 'navigointi.html'; ?>  
+<div class="content">
+<h1>Koulutusliike Talli</h1>
+  <p>Tervetuloa Koulutusliike Talli kotisivuille! Täältä löydät tietoa meiden koulutuksista ja rekrytointiohjelmistamme.</p>
+  <h1>Koulutukset</h1>
+    <p>Tarjoamme paljon erillaisia koulutuksia kaiken tasoisille oppillaille tai halukkaille</p>
+    
+</div>
+<?php include 'footer.html'; ?>
+</body>
+</html>
