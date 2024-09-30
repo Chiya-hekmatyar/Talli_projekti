8c27014eeb0b15be2f2ec3779dbd7d1ec0395b5e
Chiya-hekmatyar <chiya.hekmatyar@edu.omnia.fi>
2024-09-30 16:30:21 +0300

    i dont know

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ include 'headers.php';
 <body>
 <?php include 'navigointi.html'; ?>  
 <div class="content">
-<h1>Koulutusliike Talli</h1>
-  <p>Tervetuloa Koulutusliike Talli kotisivuille! Täältä löydät tietoa meiden koulutuksista ja rekrytointiohjelmistamme.</p>
-  <h1>Koulutukset</h1>
-    <p>Tarjoamme paljon erillaisia koulutuksia kaiken tasoisille oppillaille tai halukkaille</p>
+<h1 class="left">Koulutusliike Talli</h1>
+  <p class="left">Tervetuloa Koulutusliike Talli kotisivuille! Täältä löydät tietoa meiden koulutuksista ja rekrytointiohjelmistamme.</p>
+  <h1 class="left">Koulutukset</h1>
+    <p class="left">Tarjoamme paljon erillaisia koulutuksia kaiken tasoisille oppillaille tai halukkaille</p>
     
 </div>
 <?php include 'footer.html'; ?>
