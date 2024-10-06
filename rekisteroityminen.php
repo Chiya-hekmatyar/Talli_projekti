b5f578fd5b9376c11586ebe1a46a0aef3512afa0
Chiya-hekmatyar <chiya.hekmatyar@edu.omnia.fi>
2024-10-06 14:46:03 +0300

    Update rekisteroityminen.php

--- rekisteroityminen.php
+++ rekisteroityminen.php
@@ -5,7 +5,7 @@ include "headers.php";
 ?>
 <body>
 <?php include "navigointi.html"; ?>
-<div class="container mt-5">
+<div class="container">
 <h2>Rekisteröityminen Tallin koulutuksiin</h2>
 <form action="sendmail.php" method="POST" class="needs-validation" novalidate>
     <legend>Henkilötiedot</legend>
