b973af3b4416a85e8f79eafc86190be9908f80da
Chiya-hekmatyar <chiya.hekmatyar@edu.omnia.fi>
2024-10-09 16:45:34 +0300

    Update yhteydenotto.php

--- yhteydenotto.php
+++ yhteydenotto.php
@@ -9,7 +9,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "datasql7.westeurope.cloudapp.azure.com";
+$servername = "datasql7.westeurope.cloudapp.azure.com?sslmode=require";
 $username = "hekmatyarch";
 $password = "73711"; 
 $dbname = "app_db";
